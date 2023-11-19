becf29e933ea93234200aa9047c3699d620b1228
Rob123450 <cedenor707@gmail.com>
2023-11-18 20:58:12 -0500

    Added a Cornell Box to the scene. Started working in ray-traced lighting, current bug related to sphere-check/light-bounces.
    Co-authored-by: Jared Reich <jaredreich@ucf.edu>

--- main.py
+++ main.py
@@ -8,6 +8,40 @@ from guiV3 import SimpleGUI
 from objLoaderV4 import ObjLoader
 import shaderLoaderV3
 import pyrr
+from utils import load_image
+
+# Loads textures
+def load_cubemap_texture(filenames):
+    # Generate a texture ID
+    texture_id = glGenTextures(1)
+
+    # Bind the texture as a cubemap
+    glBindTexture(GL_TEXTURE_CUBE_MAP, texture_id)
+
+    # Define texture parameters
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST)
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+
+    # Define the faces of the cubemap
+    faces = [GL_TEXTURE_CUBE_MAP_POSITIVE_X, GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+             GL_TEXTURE_CUBE_MAP_POSITIVE_Y, GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+             GL_TEXTURE_CUBE_MAP_POSITIVE_Z, GL_TEXTURE_CUBE_MAP_NEGATIVE_Z]
+
+    # Load and bind images to the corresponding faces
+    for i in range(6):
+        img_data, img_w, img_h = load_image(filenames[i], format="RGB", flip=False)
+        glTexImage2D(faces[i], 0, GL_RGB, img_w, img_h, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
+
+    # Generate mipmaps
+    glGenerateMipmap(GL_TEXTURE_CUBE_MAP)
+
+    # Unbind the texture
+    glBindTexture(GL_TEXTURE_CUBE_MAP, 0)
+
+    return texture_id
 
 # Does all the vao/vbo related stuff in a function
 # Can use this to bind all the buffers for any object drawn in the scene
@@ -39,9 +73,12 @@ def input_handler():
     global eye
     global camera_forward
     global deltaTime
+    global yaw
+    global pitch
     pressed_inputs = pg.key.get_pressed()
 
     cameraSpeed = 2.5 * deltaTime
+    mouseSens = 0.5
     if (pressed_inputs[pg.K_w] or pressed_inputs[pg.K_UP]):
         eye += cameraSpeed * camera_forward
     
@@ -54,6 +91,23 @@ def input_handler():
     if (pressed_inputs[pg.K_d] or pressed_inputs[pg.K_RIGHT]):
         eye += (np.cross(camera_forward, up)) * cameraSpeed
 
+    dx, dy = pg.mouse.get_rel()
+    yaw += mouseSens * dx
+    pitch -= mouseSens * dy
+
+    if (pitch > 89):
+        pitch = 89
+
+    if (pitch < -89):
+        pitch = -89
+
+    forward = np.array([0, 0, 0,], dtype=np.float32)
+    forward[0] = np.cos(np.radians(yaw)) * np.cos(np.radians(pitch))
+    forward[1] = np.sin(np.radians(pitch))
+    forward[2] = np.sin(np.radians(yaw)) * np.cos(np.radians(pitch))
+    camera_forward = forward
+    
+
 
 # PROGRAM START
 # Initialize pygame
@@ -64,8 +118,10 @@ pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
 pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
 
 # Create a window for graphics using OpenGL
-width = 900
-height = 500
+# width = 900
+# height = 500
+width = 1920
+height = 1080
 pg.display.set_mode((width, height), pg.OPENGL | pg.DOUBLEBUF)
 
 
@@ -74,24 +130,29 @@ glEnable(GL_DEPTH_TEST)
 
 
 # Write our shaders. We will write our vertex shader and fragment shader in a different file
-shaderProgram = shaderLoaderV3.ShaderProgram("shaders/vert.glsl", "shaders/frag.glsl")
+shaderProgram = shaderLoaderV3.ShaderProgram("shaders/obj/vert.glsl", "shaders/obj/frag.glsl")
+shaderProgram_skybox = shaderLoaderV3.ShaderProgram("shaders/skybox/vert.glsl", "shaders/skybox/frag.glsl")
+shaderProgram_sphere = shaderLoaderV3.ShaderProgram("shaders/sphere/vert.glsl", "shaders/sphere/frag.glsl")
 
 # Camera parameters
 eye = np.array([0,0,6], dtype=np.float32)
 camera_forward = np.array([0, 0, -1], dtype=np.float32)
 up = np.array([0,1,0], dtype=np.float32)
 
+yaw = -90.0
+pitch = 0
+
 fov = 45
 aspect = width/height
-near = 2
-far = 20
+near = 0.1
+far = 100
 
 view_mat  = pyrr.matrix44.create_look_at(eye, camera_forward, up)
 projection_mat = pyrr.matrix44.create_perspective_projection_matrix(fov, aspect, near, far)
 
 # light and material properties
 material_color = (1.0, 0.1, 0.1)
-light_pos = np.array([2, 2, 2, None], dtype=np.float32)
+light_pos = np.array([2, 1, 2, None], dtype=np.float32)
 # last component is for light type (0: directional, 1: point) which is changed by radio button
 # *************************************************************************
 
@@ -99,42 +160,96 @@ light_pos = np.array([2, 2, 2, None], dtype=np.float32)
 # Lets load our objects
 obj = ObjLoader("objects/sphere.obj")
 obj_plane = ObjLoader("objects/square.obj")
+obj_cube = ObjLoader("objects/cube.obj")
 
+obj_cube_scale = 0.5 / obj_cube.dia
 
 # *********** Lets define model matrix ***********
 translation_mat = pyrr.matrix44.create_from_translation(-obj.center)
 scaling_mat = pyrr.matrix44.create_from_scale([2 / obj.dia, 2 / obj.dia, 2 / obj.dia])
-model_mat = pyrr.matrix44.multiply(translation_mat, scaling_mat)
+model_mat = pyrr.matrix44.multiply(scaling_mat, translation_mat)
 
-# *********** Defining quad matrix ***********
+
+# *********** Defining light sphere ***********
+translation_mat = pyrr.matrix44.create_from_translation(light_pos)
+scaling_mat = pyrr.matrix44.create_from_scale([2 / obj.dia, 2 / obj.dia, 2 / obj.dia])
+model_mat_light_sphere = pyrr.matrix44.multiply(scaling_mat, translation_mat)
+
+# *********** Defining ground plane ***********
 rotation_mat = pyrr.matrix44.create_from_x_rotation(np.deg2rad(90))
 translation_mat = pyrr.matrix44.create_from_translation([0, -1, 0])
-scaling_mat = pyrr.matrix44.create_from_scale([2, 2, 2])
+scaling_mat = pyrr.matrix44.create_from_scale([100, 100, 100])
 model_mat_plane = pyrr.matrix44.multiply(scaling_mat, rotation_mat)
 model_mat_plane = pyrr.matrix44.multiply(model_mat_plane, translation_mat)
 
+# *********** Defining top wall cube ***********
+rotation_mat = pyrr.matrix44.create_from_x_rotation(np.deg2rad(-90))
+translation_mat = pyrr.matrix44.create_from_translation([0, 2 + obj_cube_scale, 0])
+scaling_mat = pyrr.matrix44.create_from_scale([2, 2, obj_cube_scale])
+model_mat_top_plane = pyrr.matrix44.multiply(scaling_mat, rotation_mat)
+model_mat_top_plane = pyrr.matrix44.multiply(model_mat_top_plane, translation_mat)
 
+# *********** Defining left wall cube ***********
+rotation_mat = pyrr.matrix44.create_from_y_rotation(np.deg2rad(90))
+translation_mat = pyrr.matrix44.create_from_translation([-2 + obj_cube_scale, 0, 0])
+scaling_mat = pyrr.matrix44.create_from_scale([2, 2, obj_cube_scale])
+model_mat_left_plane = pyrr.matrix44.multiply(scaling_mat, rotation_mat)
+model_mat_left_plane = pyrr.matrix44.multiply(model_mat_left_plane, translation_mat)
+
+# *********** Defining right wall cube ***********
+rotation_mat = pyrr.matrix44.create_from_y_rotation(np.deg2rad(90))
+translation_mat = pyrr.matrix44.create_from_translation([2 - obj_cube_scale, 0, 0])
+scaling_mat = pyrr.matrix44.create_from_scale([2, 2, obj_cube_scale])
+model_mat_right_cube = pyrr.matrix44.multiply(scaling_mat, rotation_mat)
+model_mat_right_cube = pyrr.matrix44.multiply(model_mat_right_cube, translation_mat)
+
+# *********** Defining back wall cube ***********
+rotation_mat = pyrr.matrix44.create_from_y_rotation(np.deg2rad(0))
+translation_mat = pyrr.matrix44.create_from_translation([0, 0, -2 + obj_cube_scale])
+scaling_mat = pyrr.matrix44.create_from_scale([2, 2, obj_cube_scale])
+model_mat_back_cube = pyrr.matrix44.multiply(scaling_mat, rotation_mat)
+model_mat_back_cube = pyrr.matrix44.multiply(model_mat_back_cube, translation_mat)
 
 # ***** Create VAO, VBO, and configure vertex attributes for object 1 *****
 # VAO
 vao_obj, vbo_obj, n_vertices_obj = build_buffers(obj)
-vao_plane, vbo_plane, n_vertices_plane = build_buffers(obj_plane)
+vao_groundPlane, vbo_groundPlane, n_vertices_plane = build_buffers(obj_plane)
+vao_topPlane, vbo_topPlane, n_vertices_topPlane = build_buffers(obj_cube)
+vao_leftPlane, vbo_leftPlane, n_vertices_leftPlane = build_buffers(obj_cube)
+vao_rightCube, vbo_rightCube, n_vertices_rightCube = build_buffers(obj_cube)
+vao_backCube, vbo_backCube, n_vertices_backCube = build_buffers(obj_cube)
+vao_lightSphere, vbo_lightSphere, n_vertices_lightSphere = build_buffers(obj)
 # *************************************************************************
+# Loading cubemap
+cubemap_images = ["images/skybox1/right.png", "images/skybox1/left.png",
+                  "images/skybox1/top.png", "images/skybox1/bottom.png",
+                  "images/skybox1/front.png", "images/skybox1/back.png"]
+cubemap_id = load_cubemap_texture(cubemap_images)
+
+shaderProgram_skybox["cubeMapTex"] = 0
 
 
-
-gui = SimpleGUI("Assignment 7")
+gui = SimpleGUI("Raytracing")
 
 # Create a slider for the rotation angle around the Z axis
 fov_slider = gui.add_slider("fov", 25, 90, 45, resolution=1)
 
 
-material_color_picker = gui.add_color_picker("material color", initial_color=material_color)
-light_type_radio_button = gui.add_radio_buttons("light type", options_dict={"point":1, "directional":0}, initial_option="point")
+lightY_slider = gui.add_slider("light Y angle", -180, 180, 0, resolution=1)
+lightX_slider = gui.add_slider("light X angle", -180, 180, 0, resolution=1)
+camY_slider = gui.add_slider("camera Y angle", -180, 180, -32, resolution=1)
+camX_slider = gui.add_slider("camera X angle", -90, 90, 13, resolution=1)
+camFov_slider = gui.add_slider("field of view", 25, 90, 35, resolution=1)
+light_color_slider = gui.add_color_picker(label_text="Light Color", initial_color=(1.0, 1.0, 1.0))
+ambient_intensity_slider = gui.add_slider("Ambient Intensity", 0, 1, 0.1, resolution=0.1)
+roughness_slider = gui.add_slider("Roughness", 0, 1, 0.5, resolution=0.01)
+metallic_slider = gui.add_slider("Metallic", 0, 1, 0.5, resolution=0.01)
+material_picker = gui.add_radio_buttons("Material", options_dict={"Iron":1, "Copper":2, "Gold":3, "Aluminum":4, "Silver":5}, initial_option="Gold")
 
 # timing
 deltaTime = 0.0
 lastFrame = 0.0
+
 
 # Run a loop to keep the program running
 draw = True
@@ -157,26 +272,107 @@ while draw:
     projection_mat = pyrr.matrix44.create_perspective_projection_matrix(fov_slider.get_value(),
                                                                         aspect, near,  far)
 
-    # Rotates the light over time, uses same sin, cos and ShaderToy example with a smaller coefficient
-    #light_pos = np.array([np.sin(timer*0.1), 1, np.cos(timer*0.1), None], dtype=np.float32)
-    light_pos[3] = light_type_radio_button.get_value()
+
+    lightYRotation = lightY_slider.get_value()
+    lightXRotation = lightX_slider.get_value()
+    light_pos[0] = np.cos(np.radians(lightYRotation)) * np.cos(np.radians(lightXRotation))
+    light_pos[1] = np.sin(np.radians(lightXRotation))
+    light_pos[2] = np.sin(np.radians(lightYRotation)) * np.cos(np.radians(lightXRotation))
 
     # Set uniforms
     shaderProgram["model_matrix"] = model_mat
     shaderProgram["view_matrix"] = view_mat
     shaderProgram["projection_matrix"] = projection_mat
     shaderProgram["eye_pos"] = eye
-    shaderProgram["material_color"] = material_color_picker.get_color()
     shaderProgram["light_pos"] = light_pos
+    shaderProgram["lightColor"] = light_color_slider.get_color()
+    shaderProgram["metallic"] = metallic_slider.get_value()
+    shaderProgram["roughness"] = roughness_slider.get_value()
+    shaderProgram["ambient_intensity"] = ambient_intensity_slider.get_value()
+    shaderProgram["mat_type"] = int(material_picker.get_value())
+    shaderProgram["center"] = obj.center
+    shaderProgram["radius"] = obj.dia / 2
+
+    glUseProgram(shaderProgram_sphere.shader)
+    glBindVertexArray(vao_obj)
+    shaderProgram_sphere["model_matrix"] = model_mat
+    shaderProgram_sphere["view_matrix"] = view_mat
+    shaderProgram_sphere["projection_matrix"] = projection_mat
+    shaderProgram_sphere["eye_pos"] = eye
+    shaderProgram_sphere["light_pos"] = light_pos
+    shaderProgram_sphere["lightColor"] = light_color_slider.get_color()
+    shaderProgram_sphere["metallic"] = metallic_slider.get_value()
+    shaderProgram_sphere["roughness"] = roughness_slider.get_value()
+    shaderProgram_sphere["ambient_intensity"] = ambient_intensity_slider.get_value()
+    shaderProgram_sphere["mat_type"] = int(material_picker.get_value())
+    shaderProgram_sphere["center"] = [0, 0, 0]
+    shaderProgram_sphere["radius"] = obj.dia / 2
+
+    glDrawArrays(GL_TRIANGLES, 0, obj.n_vertices)      # draw the object
 
     glUseProgram(shaderProgram.shader)
 
-    glBindVertexArray(vao_obj)
-    glDrawArrays(GL_TRIANGLES,0, obj.n_vertices)      # draw the object
-
     shaderProgram["model_matrix"] = model_mat_plane
-    glBindVertexArray(vao_plane)
+    shaderProgram["mat_type"] = 1
+    glBindVertexArray(vao_groundPlane)
+    glDrawArrays(GL_TRIANGLES, 0, obj_plane.n_vertices)
+
+    # Drawing Top Cube
+    shaderProgram["metallic"] = 0.0
+    shaderProgram["roughness"] = 1.00
+    shaderProgram["mat_type"] = 3
+    shaderProgram["model_matrix"] = model_mat_top_plane
+    glBindVertexArray(vao_topPlane)
+    glDrawArrays(GL_TRIANGLES, 0, obj_cube.n_vertices)
+
+    # Drawing Left Cube
+    shaderProgram["metallic"] = 0.0
+    shaderProgram["roughness"] = 1.00
+    shaderProgram["mat_type"] = 2
+    shaderProgram["model_matrix"] = model_mat_left_plane
+    glBindVertexArray(vao_leftPlane)
+    glDrawArrays(GL_TRIANGLES, 0, obj_cube.n_vertices)
+
+    # Drawing Right Cube
+    shaderProgram["metallic"] = 0.0
+    shaderProgram["roughness"] = 1.00
+    shaderProgram["mat_type"] = 5
+    shaderProgram["model_matrix"] = model_mat_right_cube
+    glBindVertexArray(vao_rightCube)
+    glDrawArrays(GL_TRIANGLES, 0, obj_cube.n_vertices)
+
+    # Drawing Back Cube
+    shaderProgram["metallic"] = 0.0
+    shaderProgram["roughness"] = 1.00
+    shaderProgram["mat_type"] = 4
+    shaderProgram["model_matrix"] = model_mat_back_cube
+    glBindVertexArray(vao_backCube)
+    glDrawArrays(GL_TRIANGLES, 0, obj_cube.n_vertices)
+
+    shaderProgram["metallic"] = 0.0
+    shaderProgram["roughness"] = 1.00
+    shaderProgram["mat_type"] = 3
+    shaderProgram["ambient_intensity"] = 1.0
+    translation_mat = pyrr.matrix44.create_from_translation(light_pos)
+    scaling_mat = pyrr.matrix44.create_from_scale([0.5 / obj.dia, 0.5 / obj.dia, 0.5 / obj.dia])
+    model_mat_light_sphere = pyrr.matrix44.multiply(scaling_mat, translation_mat)
+    shaderProgram["model_matrix"] = model_mat_light_sphere
+    glBindVertexArray(vao_lightSphere)
     glDrawArrays(GL_TRIANGLES, 0, obj.n_vertices)
+
+    # Drawing the cube map
+    # view_mat_without_translation = view_mat.copy()
+    # view_mat_without_translation[3][:3] = [0,0,0]
+
+    # # compute the inverse of the view (one without translation)- projection matrix
+    # inverseViewProjection_mat = pyrr.matrix44.inverse(pyrr.matrix44.multiply(view_mat_without_translation,projection_mat))
+    # glDepthFunc(GL_LEQUAL)
+    # glUseProgram(shaderProgram_skybox.shader)
+    # shaderProgram_skybox["invViewProjectionMatrix"] = inverseViewProjection_mat
+    # glBindVertexArray(vao_quad)
+    # glDrawArrays(GL_TRIANGLES, 0, obj_plane.n_vertices)
+
+
     # ****************************************************************************************************
 
 
@@ -185,8 +381,8 @@ while draw:
 
 
 # Cleanup
-glDeleteVertexArrays(1, [vao_obj, vao_plane])
-glDeleteBuffers(1, [vbo_obj, vbo_plane])
+glDeleteVertexArrays(1, [vao_obj, vao_groundPlane])
+glDeleteBuffers(1, [vbo_obj, vbo_groundPlane])
 glDeleteProgram(shaderProgram.shader)
 
 pg.quit()   # Close the graphics window
---
+++ shaders/obj/frag.glsl
@@ -0,0 +1,193 @@
+#version 420 core
+#define NUMBOUNCES 1000
+#define PI 3.14159
+#define EPSILON 1e-5
+#define INFINITY 1e5
+
+out vec4 outColor;
+
+in vec3 fragNormal;
+in vec3 fragPosition;
+
+uniform vec3 center;
+uniform float radius;
+
+//PBR Uniforms
+uniform vec4 light_pos;
+uniform vec3 eye_pos;
+uniform float ambient_intensity;
+uniform float roughness;
+uniform float metallic;
+uniform int mat_type;
+uniform vec3 lightColor;
+uniform vec3 material_color;
+// Raytracing Uniforms
+
+uniform vec3 Background;
+uniform vec2 resolution;
+uniform vec3 cameraU;
+uniform vec3 cameraV;
+uniform vec3 cameraW;
+uniform vec3 cameraEye;
+uniform float fov;
+
+uniform vec3 minBound;
+uniform vec3 maxBound;
+
+
+
+struct Hit{
+    float d;
+    vec3 point;
+    vec3 normal;
+};
+
+struct Ray{
+    vec3 direction;
+    vec3 origin;
+    float tMin, tMax;
+};
+
+struct AABB {
+      vec3 minP;
+      vec3 maxP;
+};
+
+vec3 pointOnRay(in Ray ray, float t){
+      return (ray.origin + t*ray.direction);
+}
+
+
+Ray getRay(vec2 pixel)
+{
+      Ray ray;
+      ray.origin = cameraEye;
+      float height = 2.*tan(fov/2.);
+      float aspect = resolution.x/resolution.y;
+      float width = height * aspect;
+      vec2 windowDim = vec2(width, height);
+      vec2 pixelSize = windowDim / resolution;
+      vec2 delta = -0.5 * windowDim + pixel * pixelSize;
+      ray.direction = -cameraW + cameraV * delta.y + cameraU * delta.x;
+      ray.tMin = 0.;
+      ray.tMax = INFINITY;
+      return ray;
+}
+
+vec3 sphereIntersectPoint(Ray ray)
+{
+      float b = 2 * dot(ray.direction, ray.origin - center);
+      float magnitude = length(ray.origin - center);
+      float c = pow(magnitude, 2) - pow(radius, 2);
+      float delta = pow(b, 2) - (4 * c);
+      if (delta > 0)
+      {
+            float t1 = (-b + sqrt(delta)) / 2;
+            float t2 = (-b - sqrt(delta)) / 2;
+
+            if (t1 > 0 && t2 > 0)
+            {
+                  return vec3(0.5);
+                  //return min(t1, t2);
+            }
+      }
+
+      return vec3(0.0);
+}
+
+
+// RaycastFrag()
+
+
+// This could potentially come later
+// Denoiser()
+
+//The following 3 Funcitons are necessary for PBR feature
+vec3 computeDiffuse(vec3 N, vec3 L, vec3 F, vec3 material_color){
+
+      vec3 ks = F;
+      vec3 Kd = 1-ks;
+
+      return  Kd * (1-metallic) * material_color * max(dot(N, L), 0);
+}
+
+float geometric_attenuation(vec3 N, vec3 V, vec3 L)
+{
+      float alpha = pow(roughness,2);
+      float k = (alpha) / 2;
+
+      // Masking Term
+      float Gv = clamp(dot(V, N), 0., 1.) / (clamp(dot(N, V), 0., 1.) * (1-k) + k);
+      // Shadowing Term
+      float Gl = clamp(dot(L, N), 0., 1.) / (clamp(dot(L, N), 0., 1.) * (1-k) + k);
+
+      return Gv * Gl;
+}
+
+float microfacet_distribution(vec3 N, vec3 H)
+{
+      float alpha = pow(roughness,2);
+      return pow(alpha, 2) / (PI * pow((pow(max(dot(H, N),0),2) * (pow(alpha,2) - 1) + 1),2));
+}
+
+
+vec3 computePBR()
+{
+      vec3 N = normalize(fragNormal);
+      vec3 L = normalize(light_pos.xyz - fragPosition);
+
+      //if (light_pos.w==0.0)   L = normalize(light_pos.xyz);                   // directional light
+      //else                    L = normalize(light_pos.xyz-fragPosition);      // point light
+
+      vec3 V = normalize(eye_pos-fragPosition);
+      vec3 H = normalize(L + V);
+
+      vec3 F0_metal;
+      vec3 F0_dielectric = vec3(.04,.04,.04);
+
+      // Metals
+      if(mat_type == 1)       F0_metal = vec3(0.56,0.57,0.58);
+      else if (mat_type == 2) F0_metal = vec3(0.95,0.64,0.54);
+      else if (mat_type == 3) F0_metal = vec3(1.00,0.71,0.29);
+      else if (mat_type == 4) F0_metal = vec3(0.91,0.92,0.92);
+      else if (mat_type == 5) F0_metal = vec3(0.95,0.93,0.88);
+
+      vec3 material_color = F0_metal;
+      vec3 F0 = mix(F0_dielectric,F0_metal,metallic);
+
+      vec3 F = F0 + (1 - F0) * pow( (1-clamp(dot(H,V), 0, 1)), 5 );
+
+      float G = geometric_attenuation(N,V,L);
+
+      float D = microfacet_distribution(N,H);
+
+      vec3 microfacet = F * D * G;
+
+      vec3 diffuseColor = computeDiffuse(N, L, F, material_color);
+
+      vec3 ambientColor = ambient_intensity * material_color;
+
+      vec3 specularColor = microfacet * lightColor;
+
+      return vec3(ambientColor + specularColor + diffuseColor);
+}
+
+
+void main()
+{
+      vec3 pbrColor = computePBR();
+
+      //outColor = vec4(ambientColor + diffuseColor + specular_color,1.0);
+
+      /*
+            Implement
+            for(int i; i < NUMBOUNCES;i++)
+                  Call raycast frag to get nearest intersected object
+                  Calculate shadows
+                  Change ray
+            Potentially multiply by diffuse color
+      */
+      
+
+      outColor = vec4(pbrColor, 1.0);
+}
